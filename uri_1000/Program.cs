bde5b55b98bfc454e04e58d8e292696e22fc552a
thiago.ssconceicao <thiago.ssconceicao@L130587923W10-1>
2022-08-30 16:16:15 -0300

    Adicionar arquivos de projeto.

---
+++ uri_1000/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello word!");
+        }
+    }
+}
